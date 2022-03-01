689bdc16cc86375135cf582c8d081f28b933efb5
Milosz Sobiesiak <miloszsobiesiak98@gmail.com>
2022-03-01 13:29:59 +0100

    first version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FirstpageComponent } from './firstpage/firstpage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstpageComponent,
   ],
   imports: [
     BrowserModule
---
+++ src/app/interfaces/menu.ts
@@ -0,0 +1,7 @@
+export interface Menu{
+    home: string;
+    education:string;
+    work:string;
+    skills:string;
+    projects:string;
+}
